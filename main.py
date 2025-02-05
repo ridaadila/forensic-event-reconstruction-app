f95bd6e4937ffcd1b2e1d35164b7c19c78a5d0d5
Rida Adila <ridaadila10@gmail.com>
2025-02-05 20:13:27 +0700

    add select columns used for episode mining function

--- main.py
+++ main.py
@@ -51,6 +51,30 @@ def clean_abstract_message(text):
         else:
             return "Unknown"
 
+def select_columns_used_for_episode_mining():   
+    base_filename = 'webserver-2019-10-05'
+    df_ori = pd.read_csv(base_filename + '-mapping.csv')
+
+    df_ori['epoch_time'] = None
+    df_ori['cleaned_message'] = None
+
+    # Konversi kolom 'datetime' ke format datetime
+    df_ori['datetime'] = pd.to_datetime(df_ori['datetime'], format='mixed')
+
+    # Mengonversi datetime ke epoch time
+    df_ori['epoch_time'] = df_ori['datetime'].apply(lambda x: int(x.timestamp()))
+
+    df_ori['cleaned_message'] = df_ori['abstract_message'].apply(clean_abstract_message)
+
+    df_ori = df_ori[df_ori['cleaned_message'] != "Unknown"]
+
+    df_ori = df_ori.sort_values(by='datetime')
+
+    columns_to_save = ['datetime', 'epoch_time', 'source', 'cluster_id', 'abstract_message', 'cleaned_message', 'message']
+
+    df_ori.to_csv(base_filename + '-selected_columns.csv', columns=columns_to_save, index=False)
+    df_ori.to_csv(base_filename + '-mapping-edited.csv', columns=['datetime', 'cluster_id', 'source', 'message', 'abstract_message'], index=False)
+
 def filter_input_timeline_by_request():
     input_file = '12-search-sql-injection.csv'
     output_file = '12-search-sql-injection-fix.csv'
